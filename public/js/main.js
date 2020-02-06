ebea220bd034cfa4b3065e7301415f6a6a0edd2b
Nesrine <bouhejbanesrine@gmail.com>
2020-02-06 07:48:38 +0100

    icones

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,6 @@ let ToDo = document.getElementsByClassName ('btn')[1]
 let Done = document.getElementsByClassName ('btn')[2]
 let Deleted = document.getElementsByClassName ('btn')[3]
 let All = document.getElementsByClassName ('btn')[4]
-let prinput = document.querySelector ('#exo-1 input')
 let divlist = document.getElementsByClassName('box-body')[0]
 
 
@@ -12,26 +11,63 @@ let divlist = document.getElementsByClassName('box-body')[0]
 var text = document.createElement("input")
 
 
-// Create a new list item when clicking on the "Add" button
-boutonajouter.addEventListener ('click', () =>{
+// Create a new list item when clicking on the "Ajouter" button
+boutonajouter.addEventListener ('click',() => {
+let prinput = document.querySelector ('#exo-1 input')
 let li = document.createElement('li')
 let ul = document.createElement('ul')
 divlist.appendChild(ul)
 ul.appendChild(li)
 li.innerText = `${prinput.value}`
 li.style.border='solid grey 1px'
+
 li.style.backgroundColor = "grey"
-li.appendChild(icone1) 
+
+li.appendChild(iconecheck) 
+li.appendChild(iconeedit)
+li.appendChild(iconetrash)
 
 let div2 = document.createElement("div")
-div2.style.marginLeft = "7"
+div2.style.marginLeft = "20"
+div2.style.justifyContent = 'right'
 li.appendChild('div2')
 
-// //icone1
-// let icone1 = document.createElement('i')
-// icone1.setAttribute('class',"far fa-check-circle")
-// icone1.style.color = 'blue'
-// icone1.style.marginRight = '100px'
+})
 
-// })
+// icone check
+
+//iconecheck
+let iconecheck = document.createElement('i')
+iconecheck.setAttribute('class',"far fa-check-circle")
+iconecheck.style.color = 'blue'
+iconecheck.style.marginRight = '100px'
+
+// icone edit
+//iconeedit
+
+let iconeedit = document.createElement('i')
+iconeedit.setAttribute('class',"far fa-edit")
+iconeedit.style.color = 'yellow'
+iconeedit.style.marginRight = '100px'
+
+//icone trash 
+//iconetrash
+
+let iconetrash = document.createElement('i')
+iconetrash.setAttribute('class',"far fa-trash-alt")
+iconetrash.style.color = 'red'
+iconetrash.style.marginRight = '100px'
+
+
+
+
+
+
+
+
+
+
+
+
+
 
